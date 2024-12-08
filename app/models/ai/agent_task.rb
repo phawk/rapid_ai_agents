5bc2ebba5226e9da8440682f5c0f33251957eed7
Pete Hawkins <petey@hey.com>
2024-12-08 07:52:53 +0000

    Remove dev log

--- app/models/ai/agent_task.rb
+++ app/models/ai/agent_task.rb
@@ -10,7 +10,6 @@ class Ai::AgentTask < ApplicationRecord
     existing = if message_params[:tool_calls].present?
       existing_tool_calls = messages.ordered.pluck(:id, :tool_calls)
       found = existing_tool_calls.select { |id, tool_calls| JSON.generate(tool_calls) == JSON.generate(message_params[:tool_calls]) }.first
-      Rails.logger.info("found with tool calls: #{found.inspect}")
       messages.find(found.first) if found.present?
     else
       messages.find_by(role: message_params[:role], content: message_params[:content], tool_call_id: message_params[:tool_call_id])
